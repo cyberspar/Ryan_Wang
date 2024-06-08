a4e315b035b6408ff638a9794ab5bddeb9cee5b4
cyberspar <wo187215@163.com>
2024-06-08 20:37:18 +0800

    website

---
+++ js/config.js
@@ -0,0 +1,29 @@
+window.config={
+    "brandTitle": "Ryan Wang",
+    "brandSubtitle": "Editor / Colorist",
+    "brandHref": "/qietu/qietu/index.html",
+    "menu": [
+        {
+            "name": "Film",
+            "href": "/qietu/qietu/index.html"
+        },
+        {
+            "name": "Commercial",
+            "href": "/qietu/qietu/index2.html"
+        },
+        {
+            "name": "Color",
+            "href": "/qietu/qietu/index3.html"
+        },
+        {
+            "name": "About",
+            "href": "/qietu/qietu/index4.html"
+        }
+    ],
+    "leftFooterTitle": "Contact",
+    "leftFooterContent": "",
+    "Facebook": "https://www.facebook.com/Yuzhouuwang/",
+    "Twitter": "",
+    "Instagram": "",
+    "Linkedin": "https://www.linkedin.com/in/yuzhou-wang-261120289/"
+}
