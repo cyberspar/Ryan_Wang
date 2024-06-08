428743bafad5fc71fe26e3bdbf62b635d7008f75
cyberspar <wo187215@163.com>
2024-06-08 21:05:14 +0800

    test

--- js/config.js
+++ js/config.js
@@ -1,23 +1,23 @@
 window.config={
     "brandTitle": "Ryan Wang",
     "brandSubtitle": "Editor / Colorist",
-    "brandHref": "Ryan_Wang/index.html",
+    "brandHref": "index.html",
     "menu": [
         {
             "name": "Film",
-            "href": "/index.html"
+            "href": "index.html"
         },
         {
             "name": "Commercial",
-            "href": "Ryan_Wang/index2.html"
+            "href": "index2.html"
         },
         {
             "name": "Color",
-            "href": "/index3.html"
+            "href": "index3.html"
         },
         {
             "name": "About",
-            "href": "/index4.html"
+            "href": "index4.html"
         }
     ],
     "leftFooterTitle": "Contact",
